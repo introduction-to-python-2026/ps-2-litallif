15d372897135d2f65c29999c461348c4e6ace000
litallif <liflital452@gmail.com>
2025-11-04 15:58:52 +0200

    Update math_utils.py
    ליטל

--- math_utils.py
+++ math_utils.py
@@ -9,7 +9,7 @@ def find_max_number(num1, num2, num3):
 def find_mean(num1, num2, num3):
     mean=(num1+ num2+num3)/3
     return mean
-
+    
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     standard_deviation=((num1-mean)**2+(num2-mean)**2+(num3-mean)**2)//3
